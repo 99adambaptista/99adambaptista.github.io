b16eaf04af94eb96a932aa33ab6902e1c8897a79
99adambaptista <60272814+99adambaptista@users.noreply.github.com>
2022-09-06 00:42:01 -0400

    Create planets.js

---
+++ jsnbody/planets.js
@@ -0,0 +1,55 @@
+//Gravitational constant
+var G = 6.67e-11;
+
+class Body {
+    //str
+    #texture;
+    //vector
+    #winSize;
+
+    //str
+    sprite;
+    //vector
+    F;
+    //str
+    img_file;
+    //double
+    R;
+    x;
+    y;
+    xVel;
+    yVel;
+    mass;
+
+    get pos() {
+        
+    }
+
+
+
+
+    /*
+    Body();
+
+    Vector2f getPos();
+
+    double getM();
+
+    void scale(Vector2u winSize, double R);
+
+    virtual void draw(RenderTarget &target, RenderStates states) const;
+
+    friend istream &operator>>(istream &in, Body &body);
+
+    void time(double time);
+
+    void move();
+
+    void setV(double ax, double ay, double time);
+
+    void setPos(double time);
+
+    ~Body();
+
+    */
+}
